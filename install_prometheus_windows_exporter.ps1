c0fc6a764a6e0ee99ec751573399cfbdc6333841
Denis Astahov <adv4000@gmail.com>
2024-05-02 07:57:47 -0700

    Add files via upload

---
+++ install_prometheus_windows_exporter.ps1
@@ -0,0 +1,16 @@
+<powershell>
+#--------------------------------------------------------------------
+# PowerShell Script to Install Prometheus Windows_Exporter on Windows
+# Tested on Windows Server 2019, 2022
+# Developed by Denis Astahov in 2024
+#--------------------------------------------------------------------
+# https://github.com/prometheus-community/windows_exporter/releases
+$WINDOWS_EXPORTER_VERSION="0.25.1"
+$URL="https://github.com/prometheus-community/windows_exporter/releases/download/v$WINDOWS_EXPORTER_VERSION/windows_exporter-$WINDOWS_EXPORTER_VERSION-amd64.msi"
+
+
+Invoke-WebRequest $URL -OutFile window_exporter.msi
+Start-Process "msiexec.exe" -ArgumentList "/i window_exporter.msi /qn" -Wait
+Remove-Item window_exporter.msi
+Get-Service windows_exporter
+</powershell>
